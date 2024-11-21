de1472c09c48cdbda561d111c7675c37e43607a2
NxKuma <neilisraelko01@gmail.com>
2024-11-21 13:44:28 +0800

    Setting Up bullet Instance

---
+++ 3rd-person-shooter/Script/bullet.gd
@@ -0,0 +1,14 @@
+extends Area3D
+
+var dir: Vector3 = Vector3()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position -= transform.basis.x * 150 * delta
+	
+
+	
+
+
+func _on_timer_timeout() -> void:
+	queue_free()
